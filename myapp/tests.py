630d1eedc79247d0dc6aa55544c33064ac41bf67
Thorinka <mmlentyshenko@gmail.com>
2023-09-11 16:15:48 +0300

    homework 25.2 done

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,168 @@
-from django.test import TestCase
+import json
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from myapp.models import Course, Lesson, Subscription
+from users.models import User
+
 
 # Create your tests here.
+
+class LessonTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(
+            email='admin@admin.ru',
+            first_name='Admin',
+            last_name='Adminov',
+            is_superuser=True,
+            is_active=True,
+            is_staff=True,
+        )
+
+        self.user.set_password('123456')
+        self.user.save()
+
+        self.course = Course.objects.create(
+            name='test',
+            description='test_description',
+        )
+
+        self.lesson = Lesson.objects.create(
+            name='test_lesson',
+            description='test_desc_lesson',
+            video='https://www.youtube.com/sdsddwdw',
+            course=self.course,
+        )
+        response = self.client.post('/users/token/', {'email': 'admin@admin.ru', 'password': '123456'})
+        token = response.json()['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+    def test_get_list(self):
+        """ Test for getting list of lessons """
+
+        response = self.client.get(
+            reverse('myapp:lesson_list')
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            response.json(),
+            {
+                "count": 1,
+                "next": None,
+                "previous": None,
+                "results": [
+                    {
+                        "id": 1,
+                        "name": "test_lesson",
+                        "course": "test"
+                    },
+                ]
+            }
+        )
+
+    def test_post_create(self):
+        """ Test for creating new lesson """
+
+        data = {
+            'name': 'test_lesson2',
+            'description': 'test_desc_lesson2',
+            'video': 'https://www.youtube.com/3123f12312',
+            'course': self.course.id
+        }
+
+        response = self.client.post(
+            '/lessons/create/',
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+    def test_lesson_update(self):
+        """ Test for patching lesson """
+
+        data = {
+            'name': 'test_lesson_updated',
+            'description': 'test_desc_lesson2_updated',
+            'video': 'https://www.youtube.com/3123f12312',
+            'course': self.course.id,
+        }
+        response = self.client.put(
+            reverse('myapp:lesson_update', args=[self.lesson.id]),
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_lesson_delete(self):
+        self.client.delete(
+            reverse('myapp:lesson_delete', args=[self.lesson.id])
+        )
+
+        self.assertFalse(Lesson.objects.filter(id=self.lesson.id).exists())
+
+
+class SubscriptionTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            email='admin@admin.ru',
+            first_name='Admin',
+            last_name='Adminov',
+            is_superuser=True,
+            is_active=True,
+            is_staff=True,
+        )
+
+        self.user.set_password('123456')
+        self.user.save()
+
+        self.course = Course.objects.create(
+            name='test',
+            description='test_description',
+        )
+
+        self.lesson = Lesson.objects.create(
+            name='test_lesson',
+            description='test_desc_lesson',
+            video='https://www.youtube.com/sdsddwdw',
+            course=self.course,
+        )
+
+        response = self.client.post('/users/token/', {'email': 'admin@admin.ru', 'password': '123456'})
+        token = response.json()['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+    def test_create_subscription(self):
+        data = {
+            "user": self.user.pk,
+            "course": self.course.pk
+        }
+        response = self.client.post(
+            reverse('myapp:subscription_create'),
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+    def test_delete_subscription(self):
+        self.client.delete(
+            reverse('myapp:subscription_delete', args=[self.course.id])
+        )
+
+        self.assertFalse(Subscription.objects.filter(id=self.course.id).exists())
--- myapp/views.py
+++ myapp/views.py
@@ -5,13 +5,14 @@ from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, generics, status
 from rest_framework.filters import OrderingFilter
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser, AllowAny
 from rest_framework.response import Response
 
-from myapp.models import Course, Lesson, Payment
+from myapp.models import Course, Lesson, Payment, Subscription
+from myapp.paginators import CoursePaginator, LessonPaginator
 from myapp.permissions import IsOwner, IsModerator
 from myapp.serializers import CourseSerializer, LessonSerializer, LessonListSerializer, PaymentSerializer, \
-    CourseDetailSerializer
+    CourseDetailSerializer, SubscriptionSerializer
 from users.models import UserRoles
 
 
@@ -22,6 +23,7 @@ class CourseViewSet(viewsets.ModelViewSet):
     default_serializer = CourseSerializer
     queryset = Course.objects.all()
     permission_classes = [IsAuthenticated]
+    pagination_class = CoursePaginator
 
     serializer_classes = {
         "retrieve": CourseDetailSerializer
@@ -62,7 +64,11 @@ class LessonCreateAPIView(generics.CreateAPIView):
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonListSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, IsOwner | IsModerator]
+    permission_classes = [IsAuthenticated, IsOwner | IsModerator | IsAdminUser]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('course', 'owner',)
+    ordering_fields = ('name',)
+    pagination_class = LessonPaginator
 
     def get_queryset(self):
         if self.request.user.is_superuser or self.request.user.is_staff or self.request.user.role == UserRoles.MODERATOR:
@@ -74,19 +80,19 @@ class LessonListAPIView(generics.ListAPIView):
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, IsOwner | IsModerator]
+    permission_classes = [IsAuthenticated, IsOwner | IsModerator | IsAdminUser]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, IsOwner | IsModerator]
+    permission_classes = [IsAuthenticated, IsOwner | IsModerator | IsAdminUser]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, IsOwner]
+    permission_classes = [IsAuthenticated, IsOwner | IsAdminUser]
 
 
 # class PaymentViewSet(viewsets.ModelViewSet):
@@ -132,3 +138,21 @@ class PaymentDestroyAPIView(generics.DestroyAPIView):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
     permission_classes = [IsAdminUser]
+
+
+
+#Subscription
+class SubscriptionCreateAPIView(generics.CreateAPIView):
+    serializer_class = SubscriptionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        new_payment = serializer.save()
+        new_payment.user = self.request.user
+        new_payment.save()
+
+
+class SubscriptionDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = SubscriptionSerializer
+    queryset = Subscription.objects.all()
+    permission_classes = [IsAdminUser | IsOwner]
